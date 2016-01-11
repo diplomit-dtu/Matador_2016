900bb602d78535d9da0ace087d84f5bfaa75d6e3
Christian <chrbudtz@gmail.com>
2016-01-11 14:35:42 +0100

    Stubs for new actions

---
+++ src/control/AskForNewGameAction.java
@@ -0,0 +1,16 @@
+package control;
+
+public class AskForNewGameAction extends Action {
+
+	public AskForNewGameAction(GameController gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
