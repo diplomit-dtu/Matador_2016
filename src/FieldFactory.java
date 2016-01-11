1598171da9779dc758c0e263aea8cbfc152b3cde
Ronnie Dalsgaard <Ronnie Dalsgaard@10.16.166.35>
2016-01-11 11:46:07 +0100

    Basic structure 1

---
+++ src/FieldFactory.java
@@ -0,0 +1,10 @@
+import java.util.Collection;
+
+public class FieldFactory {
+
+	public static Collection<? extends Field> makeFields() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
