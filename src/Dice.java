cf9ec3acd061cdec6f430032f4f4bbdbd9d7c003
Ronnie Dalsgaard <Ronnie Dalsgaard@10.16.166.35>
2016-01-11 11:21:35 +0100

    Add Dice

---
+++ src/Dice.java
@@ -0,0 +1,14 @@
+
+public class Dice {
+	private int value;
+	
+	public Dice(){
+		roll();
+	}
+
+	public void roll() {
+		value = (int)(Math.random()*6 + 1);
+	}
+	
+	public int getValue(){ return value; }
+}
